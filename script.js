4f9b9f15e7b06646fd2affa34ecd172aadfec1bf
Olivia Letendart <letendartolivia@gmail.com>
2020-01-15 10:48:34 +0100

    rectification needles

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function setDate() {
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + 90;
-  minHand.style.transform = `rotate(${minsDegrees})`;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getMinutes();
   const hourDegrees = (mins / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hourDegrees})`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
