562a13c26914f0306d47bd11eb4e970e2394d6ac
Olivia Letendart <letendartolivia@gmail.com>
2020-01-15 10:45:18 +0100

    create new const min + hour and create const rotate needles

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 "use strict";
 
 const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector('.hour-hand')
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360;
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+minHand.style.transform = `rotate(${})`
 }
 
 setInterval(setDate, 1000);
