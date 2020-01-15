cbd5e64fc767fe9371b51cfee2e7497eaf6a0365
Olivia Letendart <letendartolivia@gmail.com>
2020-01-15 10:38:54 +0100

    create new const secondHand

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const secondHand = document.querySelector(".second-hand");
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
 }
 
 setInterval(setDate, 1000);
